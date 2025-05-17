cb394603523a29164f648d2b056635e8a0b8b721
Đạt Nguyễn <thanhdatvn11@gmail.com>
2025-05-17 20:15:10 +0000

    refactor: update 17

--- src/17. Letter Combinations of a Phone Number/solve.ts
+++ src/17. Letter Combinations of a Phone Number/solve.ts
@@ -1,7 +1,7 @@
-function letterCombinations(digits: string): string[] {
-  if (!digits.length) return [];
+import { checkTestcases } from "../_utils/helper";
 
-  const mapper: Record<string, string> = {
+function letterCombinations(digits: string): string[] {
+  const map: Record<string, string> = {
     "2": "abc",
     "3": "def",
     "4": "ghi",
@@ -11,22 +11,42 @@ function letterCombinations(digits: string): string[] {
     "8": "tuv",
     "9": "wxyz",
   };
-
   const result: string[] = [];
 
-  function dfs(startIndex = 0, path = "") {
-    if (path.length == digits.length) {
-      result.push(path);
+  function dfs(startDigit: number, path: string[]) {
+    if (startDigit >= digits.length) {
+      result.push(path.join(""));
       return;
     }
 
-    for (const char of mapper[digits[startIndex]]) {
-      dfs(startIndex + 1, path + char);
+    const digit = digits[startDigit];
+    for (let i = 0; i < map[digit].length; i++) {
+      path.push(map[digit][i]);
+      dfs(startDigit + 1, path);
+      path.pop();
     }
   }
 
-  dfs();
+  if (digits.length) dfs(0, []);
+
   return result;
 }
 
-console.log(letterCombinations("23"));
+checkTestcases(
+  [
+    {
+      input: ["23"],
+      expected: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"],
+    },
+    {
+      input: [""],
+      expected: [],
+    },
+    {
+      input: ["2"],
+      expected: ["a", "b", "c"],
+    },
+  ],
+  letterCombinations,
+  (a, b) => JSON.stringify(a.sort()) == JSON.stringify(b.sort())
+);
