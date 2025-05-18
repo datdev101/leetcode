b41f36e1e9a9565a45f8200ce7e2a7d36044a9de
Đạt Nguyễn <thanhdatvn11@gmail.com>
2025-05-18 08:59:57 +0000

    refactor: update 46

--- src/46. Permutations/solve.ts
+++ src/46. Permutations/solve.ts
@@ -1,27 +1,63 @@
+import { checkTestcases } from "../_utils/helper";
+
 function permute(nums: number[]): number[][] {
-  const path: number[] = [];
-  const visited = Array(nums.length).fill(0);
+  const minNum = -10;
+  const maxNum = 10;
+  const container = Array(maxNum - minNum + 1).fill(0);
+
+  for (const num of nums) {
+    container[num - minNum]++;
+  }
+
   const result: number[][] = [];
 
-  function dfs() {
-    if (path.length == nums.length) {
+  function dfs(path: number[]) {
+    if (path.length >= nums.length) {
       result.push([...path]);
       return;
     }
 
-    for (let i = 0; i < nums.length; i++) {
-      if (visited[i]) continue;
+    for (let i = minNum; i <= maxNum; i++) {
+      if (container[i - minNum] == 0) continue;
 
-      path.push(nums[i]);
-      visited[i] = 1;
-      dfs();
+      container[i - minNum]--;
+      path.push(i);
+      dfs(path);
       path.pop();
-      visited[i] = 0;
+      container[i - minNum]++;
     }
   }
 
-  dfs();
+  dfs([]);
+
   return result;
 }
 
-console.log(JSON.stringify(permute([1, 2, 3])));
+checkTestcases(
+  [
+    {
+      input: [[1, 2, 3]],
+      expected: [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1],
+      ],
+    },
+    {
+      input: [[0, 1]],
+      expected: [
+        [0, 1],
+        [1, 0],
+      ],
+    },
+    {
+      input: [[1]],
+      expected: [[1]],
+    },
+  ],
+  permute,
+  (a, b) => JSON.stringify(a) == JSON.stringify(b)
+);
